331c081159c31135fb5788fb4c65dd0cf858d718
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-04-25 21:52:46 +0700

    Updates

---
+++ precache-manifest.441a40e1eacdfe66dafa9d4fb68355ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec231a9c743804f38b6ab24eb1e76145",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "d0b8b9819da7c74c886a",
+    "url": "/TodoApp/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "748ec828575eb5046018",
+    "url": "/TodoApp/static/css/main.d4821dac.chunk.css"
+  },
+  {
+    "revision": "d0b8b9819da7c74c886a",
+    "url": "/TodoApp/static/js/2.5277abb0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TodoApp/static/js/2.5277abb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748ec828575eb5046018",
+    "url": "/TodoApp/static/js/main.fab433cc.chunk.js"
+  },
+  {
+    "revision": "45c50e49cc3cc4bc1722",
+    "url": "/TodoApp/static/js/runtime-main.8c50e0be.js"
+  },
+  {
+    "revision": "8b055f01b73ad5dc771b2428fdecc34e",
+    "url": "/TodoApp/static/media/Cuprum-Regular-webfont.8b055f01.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
